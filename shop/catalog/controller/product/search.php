80f535de74dba4883a52a7565d669e816f7dd6fd
irwan ibrahim <irwanbibrahim@onewoorks.com>
2022-06-06 03:09:37 +0800

    search as latest

--- shop/catalog/controller/product/search.php
+++ shop/catalog/controller/product/search.php
@@ -220,9 +220,9 @@ class ControllerProductSearch extends Controller {
 				} else {
 					$rating = false;
 				}
-
 				$data['products'][] = array(
 					'product_id'  => $result['product_id'],
+					'stock_stat'  => $result['stock_status'],
 					'thumb'       => $image,
 					'name'        => $result['name'],
 					'description' => utf8_substr(trim(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8'))), 0, $this->config->get('theme_' . $this->config->get('config_theme') . '_product_description_length')) . '..',
@@ -231,7 +231,8 @@ class ControllerProductSearch extends Controller {
 					'tax'         => $tax,
 					'minimum'     => $result['minimum'] > 0 ? $result['minimum'] : 1,
 					'rating'      => $result['rating'],
-					'href'        => $this->url->link('product/product', 'product_id=' . $result['product_id'] . $url)
+					'href'        => $this->url->link('product/product', 'product_id=' . $result['product_id'] . $url),
+					'tag'         => $result['tag']
 				);
 			}
 
